8ad41f40231fc3195e191e0dd6140234d42920ad
fredsazy <iriafredrick16@gmail.com>
2023-09-26 07:18:09 -0700

    first commit

---
+++ fredsazy_drf/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from core import views as core_views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view())
+]
